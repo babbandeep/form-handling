451ad85e88d122d650eb7d8ab0561ce298687f66
babbandeep <babangurdaspur@gmail.com>
2017-07-11 09:53:46 +0530

    Create getuser.php

---
+++ getuser.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<?php
+
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+
+$con = mysqli_connect($dbhost, $dbuser, $dbpass);
+
+if (!mysqli_select_db($con, 'my_db')){
+    echo "error connecting";
+}
+$q = $_GET['q'];
+
+mysqli_select_db($con,'my_db');
+$sql="SELECT * FROM tableform ORDER BY $q";
+$result = mysqli_query($con,$sql);
+
+echo "<table>
+<tr>
+<th>Id</th>
+<th>Name</th>
+<th>Email</th>
+<th>Phone</th>
+<th>Message</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "<td>" . $row['phone'] . "</td>";
+    echo "<td>" . $row['message'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+?>
+</body>
+</html>
