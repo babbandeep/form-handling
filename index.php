61a451766dd621ed41d4ffdfc0574c2889635eda
babbandeep <babangurdaspur@gmail.com>
2017-07-11 09:49:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+		    <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="css/bootstrap.css">
+            <link rel="stylesheet" href="css/bootstrap.min.css">
+			<link rel="stylesheet" href="style.css">
+	        <link rel="stylesheet" href="font-awe/css/font-awesome.min.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		</head>
+		<body>
+		    <div class="container-fluid  banner1">
+			    <div class="row">
+				    <center><h1 class="he1">OPTIONS</h1></center>
+				</div>
+			</div>
+			<div class="container-fluid banner2">
+			    <div class="row ">
+				    <div class="col-sm-12 hi">
+					   <a href="form.php"><h3 class="he2">Input New Data</h3></a>
+					   <hr style="background-color:white">
+					</div>
+					<div class="col-sm-12 hi">
+					   <a href="view2.php"><h3 class="he2">View Data</h3></a>
+					   <hr style="background-color:white">
+					</div>
+					
+					<div class="col-sm-12 hi">
+					   <a href="delete.php"><h3 class="he2">Delete Data</h3></a>
+					   <hr style="background-color:white">
+					</div>
+				</div>
+			</div>
+		</body>
+	</html>
